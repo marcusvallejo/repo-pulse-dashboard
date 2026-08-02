c3af4235d7d48a67b41f1a5487610899487157c1
yooh2 <yooh_1@outlook.com>
2026-08-02 18:07:28 +0100

    Use constant for repository data URL

--- react-version/src/hooks/useRepositoryData.js
+++ react-version/src/hooks/useRepositoryData.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+const REPOSITORY_DATA_URL = "/data/repositories.json";
+
 function useRepositoryData() {
   const [isLoading, setIsLoading] = useState(true);
   const [repositoryData, setRepositoryData] = useState(null);
@@ -8,7 +10,7 @@ function useRepositoryData() {
   useEffect(function () {
     async function loadRepositoryData() {
       try {
-        const response = await fetch("/data/repositories.json");
+        const response = await fetch(REPOSITORY_DATA_URL);
 
         if (!response.ok) {
           throw new Error("Could not load repository data");
