8b0b018cf15ec6dfb34097627a9ab3b1a007e8ec
yooh2 <yooh_1@outlook.com>
2026-08-28 12:20:59 +0100

    Moved repository data logic into service

--- server/routes/repositories.js
+++ server/routes/repositories.js
@@ -1,46 +1,33 @@
 const express = require("express");
-const repositories = require("../data/repositories.json");
+const repositoryService = require("../services/repositoryService");
 
 const router = express.Router();
 
 function sendRepositoryNotFound(response) {
   return response.status(404).json({
     error: "Repository not found",
-    availableRepositories: Object.keys(repositories),
+    availableRepositories: repositoryService.getAvailableRepositoryIds(),
   });
-}
-
-function findMetricValue(repository, label) {
-  const metric = repository.metrics.find(function (metric) {
-    return metric.label === label;
-  });
-
-  return metric?.value;
 }
 
 router.get("/", function (request, response) {
-  response.json(repositories);
+  response.json(repositoryService.getAllRepositories());
 });
 
 router.get("/:repositoryId/summary", function (request, response) {
   const repositoryId = request.params.repositoryId;
-  const repository = repositories[repositoryId];
+  const repository = repositoryService.getRepositoryById(repositoryId);
 
   if (!repository) {
     return sendRepositoryNotFound(response);
   }
 
-  response.json({
-    id: repositoryId,
-    openPullRequests: findMetricValue(repository, "Open PRs"),
-    commits: findMetricValue(repository, "Commits"),
-    healthScore: findMetricValue(repository, "Health score"),
-  });
+  response.json(repositoryService.getRepositorySummary(repositoryId));
 });
 
 router.get("/:repositoryId", function (request, response) {
   const repositoryId = request.params.repositoryId;
-  const repository = repositories[repositoryId];
+  const repository = repositoryService.getRepositoryById(repositoryId);
 
   if (!repository) {
     return sendRepositoryNotFound(response);
---
+++ server/services/repositoryService.js
@@ -0,0 +1,39 @@
+const repositories = require("../data/repositories.json");
+
+function getAllRepositories() {
+  return repositories;
+}
+
+function getAvailableRepositoryIds() {
+  return Object.keys(repositories);
+}
+
+function getRepositoryById(repositoryId) {
+  return repositories[repositoryId];
+}
+
+function findMetricValue(repository, label) {
+  const metric = repository.metrics.find(function (metric) {
+    return metric.label === label;
+  });
+
+  return metric?.value;
+}
+
+function getRepositorySummary(repositoryId) {
+  const repository = getRepositoryById(repositoryId);
+
+  return {
+    id: repositoryId,
+    openPullRequests: findMetricValue(repository, "Open PRs"),
+    commits: findMetricValue(repository, "Commits"),
+    healthScore: findMetricValue(repository, "Health score"),
+  };
+}
+
+module.exports = {
+  getAllRepositories,
+  getAvailableRepositoryIds,
+  getRepositoryById,
+  getRepositorySummary,
+};
