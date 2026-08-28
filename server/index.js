b158dd5a5df5a23a08eb57c43b12e0838c335641
yooh2 <yooh_1@outlook.com>
2026-08-28 11:43:24 +0100

    Added repository summary API route

--- server/index.js
+++ server/index.js
@@ -15,6 +15,25 @@ app.get("/api/repositories", function (request, response) {
   response.json(repositories);
 });
 
+app.get("/api/repositories/:repositoryId/summary", function (request, response) {
+  const repositoryId = request.params.repositoryId;
+  const repository = repositories[repositoryId];
+
+  if (!repository) {
+    return response.status(404).json({
+      error: "Repository not found",
+      availableRepositories: Object.keys(repositories),
+    });
+  }
+
+  response.json({
+    id: repositoryId,
+    openPullRequests: repository.metrics[0].value,
+    commits: repository.metrics[2].value,
+    healthScore: repository.metrics[3].value,
+  });
+});
+
 app.get("/api/repositories/:repositoryId", function (request, response) {
   const repositoryId = request.params.repositoryId;
   const repository = repositories[repositoryId];
